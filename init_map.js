733336001915098034499e7ffd2da059525ce8e6
Jake Cracknell <drspa@ymail.com>
2018-10-04 20:54:56 +0100

    started work on showing subset of bus lines

--- init_map.js
+++ init_map.js
@@ -1,5 +1,8 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiemV0dGVyIiwiYSI6ImVvQ3FGVlEifQ.jGp_PWb6xineYqezpSd7wA';
 
+let allGeojson;
+let highlightedGeojson = {"type": "FeatureCollection", "features": []};
+
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10',
@@ -8,13 +11,24 @@ var map = new mapboxgl.Map({
 });
 
 map.on('load', function() {
+    $.getJSON('bus_routes.json', onGeojsonLoaded);
+});
+
+map.on('click', function() {
+    highlightedGeojson.features = allGeojson.features.filter(f => f.properties.line === "33");
+    map.getSource("bus_routes").setData(highlightedGeojson);
+});
+
+function onGeojsonLoaded(data) {
+    allGeojson = data;
     map.addSource('bus_routes', {
         type: 'geojson',
-        data: 'bus_routes.json'
+        data: allGeojson
     });
     map.addLayer({
         id: 'bus_routes',
         type: 'line',
         source: 'bus_routes'
     });
-});
+}
+
